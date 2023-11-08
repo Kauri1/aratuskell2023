654d295ada19851d3452a14198fdaa9c96d8637a
Kauri <kauriremm@gmail.com>
2023-11-08 13:26:41 +0200

    Tkinter-i kasutus nüüd, teise GUI kustutamine, enam ei mäletagi selle nim

--- tkinter_näide.py
+++ tkinter_näide.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 
-
 root = tk.Tk()
 
 #gui suurus ei ole vajalik
@@ -15,9 +14,50 @@ label.pack()
 
 #kasutaja saab kirjutada
 textbox = tk.Text(root, font=("Arial", 16), height=3)
-#kasti kuvamine
-textbox.pack()
+#kasti kuvamine padx-kui palju ruumi(px) servadest(x kordinaat) vaba
+textbox.pack(padx=10)
+
+#1 rida väike kirjutusala
+myentry = tk.Entry(root)
+myentry.pack()
+
+#nupp
+button = tk.Button(root, text="Click Me!", font=("Arial", 16))
+button.pack(pady=10)
+
+#gridi tegemine
+buttonframe = tk.Frame(root)
+buttonframe.columnconfigure(0, weight=1)
+buttonframe.columnconfigure(1, weight=1)
+buttonframe.columnconfigure(2, weight=1)
+
+#paneme root asemel buttonframe, kuna see nagu uus ekraan
+btn1 = tk.Button(buttonframe, text="1", font=("Arial", 18))
+btn1.grid(row=0, column=0, sticky="we")
+
+btn2 = tk.Button(buttonframe, text="2", font=("Arial", 18))
+btn2.grid(row=0, column=1, sticky="we")
+
+btn3 = tk.Button(buttonframe, text="3", font=("Arial", 18))
+btn3.grid(row=0, column=2, sticky="we")
+
+btn4 = tk.Button(buttonframe, text="4", font=("Arial", 18))
+btn4.grid(row=1, column=0, sticky="we")
+
+btn5 = tk.Button(buttonframe, text="5", font=("Arial", 18))
+btn5.grid(row=1, column=1, sticky="we")
+
+btn6 = tk.Button(buttonframe, text="6", font=("Arial", 18))
+btn6.grid(row=1, column=2, sticky="we")
+
+#lisa grid lehele
+buttonframe.pack(fill="x")
+
+
+anotehrbtn = tk.Button(root, text="test")
+#saab panna elemente kordinaatide järgi
+anotehrbtn.place(x=100,y=200, height = 100, width=100)
+
 
 root.mainloop()
-
 
---
+++ tkinter_näide2.py
@@ -0,0 +1,52 @@
+import tkinter as tk
+from tkinter import messagebox
+
+
+#ei tea mis class teeb, lihtsam siin suhelda GUI ja programmi vahel
+class MyGUI:
+    def __init__(self):
+        #root, algus
+        self.root = tk.Tk()
+        
+        self.label = tk.Label(self.root, text="Your Message", font=("Arial", 18))
+        self.label.pack(padx=10,pady=10)
+        #Saab lugeda nupu vajutust
+        self.textbox = tk.Text(self.root, height=5, font=("Arial", 18))
+        
+        self.textbox.bind("<KeyPress>", self.shortcut)
+        self.textbox.pack(padx=10, pady=10)
+        
+        self.check_state = tk.IntVar()
+        
+        self.check = tk.Checkbutton(self.root, text="Show Messagebox", font=("Arial", 16), variable = self.check_state)
+        self.check.pack(padx=10, pady=10)
+        
+        #show message siin ei kutsu välja funktsiooni, vaid kasutab seda muutujana
+        self.button = tk.Button(self.root, text="Show Message", font=("Arial", 18), command = self.show_message)
+        self.button.pack(padx=10, pady=10)
+        
+        
+        #mainloop, lõpp
+        self.root.mainloop()
+    
+    def show_message(self):
+        #print("Hello World")
+        #self.check_state.get() annab check_state väärtuse
+        if self.check_state.get() == 0:
+            #self.textbox.get("1.0", tk.END) saab textboxi väärtuse, 1.0 alusta algusest, tk.END tähendab lõpp
+            print(self.textbox.get("1.0", tk.END))
+        else:
+            #messagebox teeb uue GUI sisestatud tekstiga
+            messagebox.showinfo(title="Message", message=self.textbox.get("1.0", tk.END))
+    
+    #kui mingi nup vajutatakse tekib event, seda saab lugeda
+    def shortcut(self, event):
+        #print(event)
+        #print(event.keysym)
+        #print(event.state)
+        #control enter kombinatsioon saadud kasutades ülemisi käske
+        if event.state == 4 and event.keysym == "Return":
+            self.show_message()
+
+MyGUI()
+    
