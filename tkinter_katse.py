1102c7dac5d4bf9c8bd3c971cac0b254317f8607
Kauri <kauriremm@gmail.com>
2023-11-09 16:19:33 +0200

    Commentite panek

--- tkinter_katse.py
+++ tkinter_katse.py
@@ -20,28 +20,39 @@ clock_label.pack()
 update_time()
 
 
-aratuse_label = tk.Label(root, text="00:00:00")
+
+
+
+#pandud aja kuvamine
+aratuse_label = tk.Label(root)
 aratuse_label.pack()
 
 
 
-#äratuse panek
+#äratuse paneku text box
 aratuse_panek = tk.Text(root, font=("Arial", 16), height=3)
 aratuse_panek.pack()
 
+#äratuskella default value
+aratuse_aeg = "00:00:00"
+aratuse_label.config(text=aratuse_aeg, font=("Arial", 100))
 
+
+#siin vahetatakse äratuse aega
 def maara_aeg():
     global aratuse_aeg
     aratuse_aeg = aratuse_panek.get("1.0", tk.END)
     aratuse_label.config(text=aratuse_aeg, font=("Arial", 100))
 
 
+
+#nupp, millega panna uus aeg sisse
 check = tk.Button(root, text="pane aeg", font=("Arial", 16), command = maara_aeg)
 check.pack()
 
-#äratuskella definitsioon
-aratuse_aeg = "00:00:00"
 
+
+#siin kontrollitakse, kas on äratus
 def aratus_kontroll():
     if current_time > aratuse_aeg:
         aratus_label.config(text="ääääärraaaaatuuuuuuuuss", font=("Arial",100))
@@ -50,7 +61,7 @@ def aratus_kontroll():
         aratus_label.config(text="maga maga", font=("Arial",100))
     root.after(1000, aratus_kontroll)
 
-#kuvab äratust
+#kuvab, kas äratus, või mitte
 aratus_label = tk.Label(root, text="maga maga", font=("Arial", 100))
 aratus_label.pack()
 
