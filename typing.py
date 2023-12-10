4c7f13967d66c86ef4426a6bffb24edf22ed89e8
Kauri <kauriremm@gmail.com>
2023-12-10 23:08:02 +0200

    typing

---
+++ typing.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+
+
+def typing():
+    root2 = tk.Tk()
+
+
+    tekst = "Siin on tekst"
+
+    tekst_label = tk.Label(root2, text=tekst, font=("Arial", 25))
+    tekst_label.pack()
+
+    
+
+
+
+
+    root2.mainloop()
+
+typing()
